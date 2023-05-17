68e4b06378d8fbc71d35e21e610f1c3cfb3f5b08
yavuz06 <yavuzselmkaya@gmail.com>
2023-05-17 15:49:44 +0300

    week9

---
+++ Week9ReadingFromFile/Program.cs
@@ -0,0 +1,56 @@
+﻿string fileDirectoryPath = @"C:\data";
+string fileName = "chuck.txt";
+
+string fullFilePath = Path.Combine(fileDirectoryPath, fileName);
+
+ReadFromFile(fullFilePath);
+
+
+static void DisplayDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+
+        Console.WriteLine(line);
+
+}
+
+static void ReadFromFile(string filePath)
+{
+    if (File.Exists(filePath))
+    {
+        string[] tempArray = File.ReadAllLines(filePath);
+        Console.WriteLine($"There are {tempArray.Length} jokes about Chuck Norris in the file");
+        Console.WriteLine("Do you want to see all lines or pick a random one?  all/random");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "all")
+        {
+            DisplayDataFromArray(tempArray);
+        }
+        else if (userChoice == "random")
+        {
+            DisplayRandomElement(tempArray);
+        }
+        else
+        {
+            Console.WriteLine("try again");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"File was not found");
+    }
+
+
+
+}
+
+
+static void DisplayRandomElement(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    Console.WriteLine(someArray[randomIndex]);
+
+
+}
